21272f57c50b17c6c0a82198982490361a969b2b
hxdegawa <hxdegawa@gmail.com>
2019-04-28 04:17:37 +0900

    Implemented champion parsing part & Japanese escape & space compatibility

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 
 const request = require('request-promise');
 const Discord = require('discord.js');
+const encoding = require('encoding-japanese');
 const client = new Discord.Client();
+const riotKey = process.env.RIOT_APIKEY;
 
 // BOT CODES
 
@@ -10,20 +12,34 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-request(`https://jp1.api.riotgames.com/lol/summoner/v4/summoners/by-name/HORTA?api_key=${process.env.RIOT_APIKEY}`)
-.then(data => getLiveMatch(JSON.parse(data).id))
-.catch(console.error);
-
-async function getLiveMatch (SummonerId) {
-  request(`https://jp1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/=${SummonerId}?api_key=${process.env.RIOT_APIKEY}`)
-  // .then(data => getLiveMatchData(JSON.parse(data).gameId))
-  .then(data => console.log(JSON.parse(data)))
-  .catch(console.error);
-}
-
-
 client.on('message', async message => {
+  const fullContents = message.content.trim().split(" ");
 
+  if (fullContents[0] === "!get" && fullContents[1]) {
+    fullContents.shift();
+    const encodedSummonerName = encoding.urlEncode(fullContents.join(" "));
+    analyzeMatchData(encodedSummonerName, 3);
+  }
+
+  async function analyzeMatchData (summonerName, count) {
+    request(`https://jp1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${riotKey}`)
+    .then(data => {return request(`https://jp1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${JSON.parse(data).id}?api_key=${riotKey}`)})
+    .then(data => parseSummonerMasterys(JSON.parse(data), count))
+    .then(data => message.channel.send(JSON.stringify(data)));
+  }
+
+  async function parseSummonerMasterys (masteryData, count) {
+    const topUsedChampions = {};
+    for (let i = 0; i < count; i ++) {
+      const key = JSON.parse(await request(`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champions/${masteryData[i].championId}.json`)).name;
+      const champion = masteryData[i];
+      topUsedChampions[key] = champion;
+      if (i === (count - 1)) {
+        return topUsedChampions;
+      }
+    }
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
+client.on('error', console.error);
