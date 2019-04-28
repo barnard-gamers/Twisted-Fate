b8e5e1c7234e77497a273b245b3dfae0bbf509ad
hxdegawa <hxdegawa@gmail.com>
2019-04-28 22:02:29 +0900

    Implemented multi-query and Styled rich-embed

--- app.js
+++ app.js
@@ -15,29 +15,99 @@ client.on('ready', () => {
 client.on('message', async message => {
   const fullContents = message.content.trim().split(" ");
 
+  if (fullContents[0] === "!get" && fullContents.join(" ").match(" -n")) {
+    fullContents.splice(0, 2);
+    const count = parseInt(fullContents[fullContents.length - 1], 10);
+
+    if (isNaN(count)) {
+      message.channel.send("You have to set a number attribute at the bottom of your request!");
+      return;
+    } else if (count > 8) {
+      message.channel.send("Sorry, The number must be lower than 9!");
+      return;
+    }
+
+    fullContents.pop();
+    const encodedSummonerName = encoding.urlEncode(fullContents.join(" "));
+    analyzeMatchData(encodedSummonerName, count, fullContents.join(" "));
+  }
+
   if (fullContents[0] === "!get" && fullContents[1]) {
     fullContents.shift();
     const encodedSummonerName = encoding.urlEncode(fullContents.join(" "));
-    analyzeMatchData(encodedSummonerName, 3);
+    analyzeMatchData(encodedSummonerName, 3, fullContents.join(" "));
   }
 
-  async function analyzeMatchData (summonerName, count) {
+  async function analyzeMatchData (summonerName, count, plainSN) {
     request(`https://jp1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${riotKey}`)
     .then(data => {return request(`https://jp1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${JSON.parse(data).id}?api_key=${riotKey}`)})
     .then(data => parseSummonerMasterys(JSON.parse(data), count))
-    .then(data => message.channel.send(JSON.stringify(data)));
+    .then(data => createRichEmbed(data, plainSN))
+    .then(data => message.channel.send(data));
   }
 
   async function parseSummonerMasterys (masteryData, count) {
     const topUsedChampions = {};
     for (let i = 0; i < count; i ++) {
-      const key = JSON.parse(await request(`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champions/${masteryData[i].championId}.json`)).name;
-      const champion = masteryData[i];
-      topUsedChampions[key] = champion;
+
+      const champion = JSON.parse(await request(`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champions/${masteryData[i].championId}.json`)).name;
+      topUsedChampions[champion] = masteryData[i];
+
       if (i === (count - 1)) {
         return topUsedChampions;
       }
     }
+  }
+
+  async function createRichEmbed (data, plainSN) {
+    const keys = Object.keys(data);
+
+    const embedMessage = new Discord.RichEmbed()
+          .setAuthor(`${plainSN}'s main champion is ${keys[0]}!`, `http://opgg-static.akamaized.net/images/lol/champion/${keys[0]}.png`)
+          .setTitle(`Champion ranking is below:`)
+          .setColor(0x4bcffa);
+
+    await keys.forEach(key => {
+      let content = {
+        "Mastery level" : data[key].championLevel,
+        "Mastery points": data[key].championPoints,
+        "Tokens owned"  : data[key].tokensEarned,
+        "op.gg URL"     : `[${key}](http://jp.op.gg/champion/${key})`
+      };
+
+      switch (content["Mastery level"]) {
+        case 5:
+          content["Mastery level"] = "mastery5";
+          break;
+
+        case 6:
+          content["Mastery level"] = "mastery6";
+          break;
+
+        case 7:
+          content["Mastery level"] = "mastery7";
+          break;
+
+        default:
+          content["Mastery level"] = data[key].championLevel;
+          break;
+      }
+
+      content = JSON.stringify(content)
+                .slice(1, JSON.stringify(content).length - 1)
+                .replace(/,/gi, "\n")
+                .replace(/"/gi, " ")
+                .replace(/:/gi, ": ")
+                .replace(/mastery5/i, message.guild.emojis.find(emoji => emoji.name === "mastery5"))
+                .replace(/mastery6/i, message.guild.emojis.find(emoji => emoji.name === "mastery6"))
+                .replace(/mastery7/i, message.guild.emojis.find(emoji => emoji.name === "mastery7"));
+
+      content += "\n";
+
+      embedMessage.addField(key, content, false);
+    }, );
+
+    return embedMessage;
   }
 });
 
